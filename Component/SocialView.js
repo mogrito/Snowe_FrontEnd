df490818480026a5809ab6d9d8e2a02999a6fd14
Je0nghunKim <rnsvhch09@naver.com>
2023-10-05 11:00:21 +0900

    1

--- Component/SocialView.js
+++ Component/SocialView.js
@@ -1,35 +1,40 @@
-import React, { useState, useContext } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, View} from 'react-native';
 import FeedList from './FeedList';
 import FloatingWriteButton from './FloatingWriteButton';
-import LogContext from '../context/LogContext';
-import { FlatList } from 'react-native-web';
 
 function SocialView() {
   const [hidden, setHidden] = useState(false);
-  const {logs} = useContext(LogContext);
+  const [boardList, setBoardList] = useState([]);
+  // 컴포넌트 마운트시 조회함수 실행 
+  useEffect(() => {
+    fetchBoardData();
+  }, []); 
 
-  const onScrolledToBottom = (isBottom) => {
+  
+  function fetchBoardData() {
+    fetch('http://localhost:8080/board/list')
+      .then(response => {
+        const boardData = response.json();
+        console.log(boardData);
+        setBoardList(boardData);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  const onScrolledToBottom = isBottom => {
     if (hidden !== isBottom) {
       setHidden(isBottom);
     }
   };
-
-  const myLogs = [
-    {
-      id: '111111',
-      title: '내 첫 번째 글',
-      body: '이것은 내 첫 번째 글입니다.',
-      date: '2023-10-03T12:00:00Z',
-    },
-    // 다른 글들도 추가할 수 있음
-  ];
+  
 
   return (
     <View style={styles.block}>
-      {/* 글 목록을 표시하는 FeedList 컴포넌트 */}
-      <FeedList logs={myLogs} onScrolledToBottom={onScrolledToBottom} /> 
-      {/* 글 작성 버튼을 표시하는 FloatingWriteButton 컴포넌트 */}
+      {/* 게시판 데이터를 FeedList 컴포넌트에 전달 */}
+      <FeedList logs={boardList} onScrolledToBottom={onScrolledToBottom} />
       <FloatingWriteButton hidden={hidden} />
     </View>
   );
