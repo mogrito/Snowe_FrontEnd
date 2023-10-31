fd73ad7ad320ce955f37bacf7834a55bbc15c731
Hee chan <kimheechan13@gmail.com>
2023-10-31 18:56:14 +0900

    10/31

--- Component/ReservationList.js
+++ Component/ReservationList.js
@@ -1,36 +1,34 @@
 import React from 'react';
 import { View, Text, FlatList, StyleSheet } from 'react-native';
+import TransparentCircleButton from './TransparentCircleButton';
+import { useNavigation } from '@react-navigation/native';
 
-
-//예약 하드코딩 데이터들 
 const data = [
-  { id: '1', name: '강사 1'},
-  { id: '2', name: '강사 2'},
-  { id: '3', name: '강사 3'},
-  { id: '4', name: '강사 4'},
-  { id: '5', name: '강사 5'},
-  { id: '6', name: '강사 6'},
-  { id: '7', name: '강사 7'},
-  { id: '8', name: '강사 8'},
-  { id: '9', name: '강사 9'},
-  { id: '10', name: '강사 10'},
-  { id: '11', name: '강사 11'} ,
-  { id: '12', name: '강사 12'},
-  { id: '13', name: '강사 13'}
+  { id: '1', name: '강사 1' },
+  { id: '2', name: '강사 2' },
+  { id: '3', name: '강사 3' },
+  // 나머지 데이터...
 ];
 
-const SkiResortPress = () => {
-
-
-
-};
-
-
-
 const ReservationListScreen = () => {
+  const navigation = useNavigation();
+
+  const onGoBack = () => {
+    navigation.pop();
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>예약 목록</Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>예약 목록</Text>
+        <View style={styles.buttonContainer}>
+          <TransparentCircleButton
+            onPress={onGoBack}
+            name="arrow-back"
+            color="#424242"
+          />
+        </View>
+      </View>
       <FlatList
         data={data}
         keyExtractor={(item) => item.id}
@@ -45,7 +43,6 @@ const ReservationListScreen = () => {
   );
 };
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -53,10 +50,18 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     paddingTop: 60,
   },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
   title: {
     fontSize: 30,
     fontWeight: 'bold',
-    marginBottom: 20,
+  },
+  buttonContainer: {
+    marginRight: 1, // 오른쪽 여백 조정
   },
   item: {
     backgroundColor: 'white',
