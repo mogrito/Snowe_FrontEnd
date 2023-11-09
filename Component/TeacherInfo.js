4617c48f0f92b7d08a00f94ac746cfdbdf0b89c7
Hee chan <kimheechan13@gmail.com>
2023-11-09 13:59:57 +0900

    11/09

--- Component/TeacherInfo.js
+++ Component/TeacherInfo.js
@@ -1,17 +1,92 @@
 import React, { useState,useEffect } from 'react';
 import { View, Text, FlatList, StyleSheet, TouchableOpacity, Image, Modal, ScrollView,} from 'react-native';
 import TransparentCircleButton from './TransparentCircleButton';
-import { useNavigation} from '@react-navigation/native';
+import Swiper from 'react-native-swiper';
+import { useNavigation } from '@react-navigation/native';
+
+//brief = 약력 carrer = 경력 team = 소속 
+
+const data = [
+  {
+    id: '1',
+    name: '원빈',
+    introduce: '강해지고 싶나?',
+    image: require('../Images/face.jpg'),
+    count: 0,
+    edudate: '09:00',
+    subject: '스키',
+    level: 'LV1',
+    yak: [
+      '- T&D(Technical & Different) HEAD COACH',
+      '- (전) 알파인 스키선수 (2007~2014)',
+      '- 건국대학교 생활체육학과 졸업',
+      '- 생활체육지도자 2급'
+    ],
+    carrer: [
+      '- KSIA 대한스키지도자연맹 전국기술선수권대회 종합 14위',
+      '- SBAK 한국스키장경영협회 전국기술선수권대회 종합 13위',
+      '- 대명비발디파크 카브배 기술선수권대회 종합 5위',
+      '- 한솔섬유배 인터스키대회 전주자'
+    ],
+    team: [
+      '- T&D SKI ',
+      '- BHS',
+      '- POC ',
+      '- LEKI'
+    ]
+  }, 
+  
+  { id: '2', name: '주성', introduce: '찾아라 원피스', image: require('../Images/face1.jpg'), count: 0, edudate: '17:00', subject: '보드', level: 'LV2' ,
+  yak: [
+    '- T&D(Technical & Different)SKI PROGRAM DIRECTOR',
+    '- SBAK 한국스키장경영협회 데몬스트레이터 3기 인정 (2018~2025)',
+    '- KSEA 대한스키교육협회 교육위원장',
+    '- KSIA 대한스키지도자연맹 데몬스트레이터 4기 인정 (2013~2017)'
+  ],
+  carrer: [
+    '- 제 8회 SBAK 한국스키장경영협회 전국기술선수권대회 종합 3위 ',
+    '- 제 4기 SBAK 한국스키장경영협회 데몬스트레이터 선발',
+    '- KSEA 대한스키교육협회 교육위원장 임명',
+    '- 한솔섬유배 인터스키대회 전주자'
+  ],
+  team: [
+    '- WONYANG 후원',
+    '- 한방유비스 후원',
+    '- OGASAKA SKI(오가사카) ',
+    '- REXXAM BOOTS(렉삼) '
+  ]},
 
 
-// const data = [
-//   { id: '1', name: '원빈', classname: '스키초급반', image: require('../Images/face.jpg'), count: 0, edudate: '09:00',subject:'스키',level:'초급' },
-//   { id: '2', name: '주성', classname: '보드초급반', image: require('../Images/face1.jpg'), count: 0, edudate: '17:00',subject:'보드',level:'중급'},
-//   { id: '3', name: '정훈', classname: '스키초급반', image: require('../Images/face2.jpg'), count: 0, edudate: '11:00',subject:'스키',level:'고급' },
-// ];
+
+  { id: '3', name: '정훈', introduce: '아이스 에이지..', image: require('../Images/face2.jpg'), count: 0, edudate: '11:00', subject: '스키', level: 'LV3',
+  yak: [
+    'T&D(Technical & Different)  COACH',
+    '- (전) 알파인 스키선수 (2007~2014)',
+    '- 용인대학교 체육학과 졸업',
+    '- 스키 경력 (12년)'
+  ],
+  carrer: [
+    '- 한솔섬유배 인터스키대회 출전',
+    '- 지산배 한국오픈스키 챔피언십 출전',
+    '- SBAK 한국스키장경영협회 전국기술선수권대회 출전',
+    '- 뉴질랜드 하계 스키캠프 참여'
+  ],
+  team: [
+    '- T&D SKI ',
+  ] },
+];
 
 
-  //DB에서 필요한 데이터는 선생님 이름 name 이랑 종목 이름 subject 반 이름 classname id는 선생님 고유 번호
+const levelColors = {
+  LV1: 'lightgreen',
+  LV2: 'lightblue',
+  LV3: 'lightpink',
+};
+
+const eachsubject = {
+  스키: require('../Images/ski.png'),
+  보드: require('../Images/skiboard.png'),
+};
 
 
 const TeacherInfoScreen = () => {
@@ -22,14 +97,50 @@ const TeacherInfoScreen = () => {
   const navigation = useNavigation();
 
   useEffect(() => {
-    fetch('선생님 데이터 받아오는 API')
-      .then((response) => response.json())
-      .then((data) => {
-        setTeachers(data); 
-      })
-      .catch((error) => console.error('Error fetching data: ', error));
-  }, []);
+    async function fetchData() {
+      try {
+        const response = await fetch(`http://localhost:8080/member/getTeacherList?ridingClass=${selectedCategory}`);
+        if (!response.ok) {
+          throw Error('서버에서 데이터를 가져오지 못했습니다.');
+        }
+        const data = await response.json();
+        setTeachers(data);
+        console.log(data);
+      } catch (error) {
+        console.error('데이터 가져오기 중 오류 발생:', error);
+      }
+    }
+    fetchData();
+  }, [selectedCategory]);
 
+ // 이부분이 선택된 강사의 DATA를 DB에서 가져오는 거
+ 
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     try {
+        
+  //       const response = await fetch('api');
+  //       if (!response.ok) {
+  //         throw Error('Failed to fetch data from the server.');
+  //       }
+  //       const data = await response.json();
+  //       setTeachers(data);
+  //     } catch (error) {
+  //       console.error('Error fetching data:', error);
+  //     }
+  //   }
+  //   fetchData();
+  // }, []);
+
+  const onShowDetails = (item) => {
+    setSelectedTeacher(item);
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+    setSelectedTeacher(null);
+  };
 
   const onGoBack = () => {
     navigation.goBack();
@@ -42,26 +153,33 @@ const TeacherInfoScreen = () => {
           <TransparentCircleButton onPress={onGoBack} name="left" color="#424242" />
           <Text style={styles.title}>강사 정보</Text>
         </View>
-
+      </View>
+      <View style={styles.categori}>
+        <TouchableOpacity style={styles.all} onPress={() => setSelectedCategory('All')}>
+          <Text style={styles.cancelButtonText}>전체</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.ski} onPress={() => setSelectedCategory('Ski')}>
+          <Text style={styles.cancelButtonText}>스키</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.board} onPress={() => setSelectedCategory('Board')}>
+          <Text style={styles.cancelButtonText}>보드</Text>
+        </TouchableOpacity>
       </View>
       <FlatList
-        data={teachers}
-        keyExtractor={(item) => item.id}
+        data={data}
         renderItem={({ item }) => (
           <View style={styles.item}>
             <View style={styles.teacherContent}>
               <Image source={item.image} style={styles.teacherImage} />
               <View style={styles.textContainer}>
                 <Text style={styles.itemText}>{item.name}</Text>
-                <View style={styles.subjectContainer}>
-                  <Text style={styles.subjectText}>{item.classname}</Text>
-                </View>
-                <Text style={styles.itemText}>{item.subject}/{item.level}</Text>
+                <Text style={styles.subjectText}>{item.introduce}</Text>
               </View>
-              <TouchableOpacity
-                onPress={() => onCancel(item.id)}
-                style={styles.cancelButton}
-              >
+              <View style={[styles.badge, { backgroundColor: levelColors[item.level] }]}>
+                <Text style={styles.skilevel}>{item.level}</Text>
+              </View>
+              <Image source={eachsubject[item.subject]} style={styles.subjectImage} />
+              <TouchableOpacity style={styles.cancelButton} onPress={() => onShowDetails(item)}>
                 <Text style={styles.cancelButtonText}>상세보기</Text>
               </TouchableOpacity>
             </View>
@@ -131,9 +249,6 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
   },
-  buttonContainer: {
-    marginRight: 1, 
-  },
   item: {
     backgroundColor: 'white',
     padding: 16,
@@ -144,10 +259,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-  },
-  imageContainer: {
-    borderRadius: 50, 
-    overflow: 'hidden', 
+    
   },
   teacherImage: {
     width: 50,
@@ -160,12 +272,7 @@ const styles = StyleSheet.create({
   },
   itemText: {
     fontSize: 16,
-    marginBottom: -5, 
-    marginTop: -5, 
-  },
-  subjectContainer: {
-    alignItems: 'center', 
-    marginBottom: 0, 
+    fontWeight: 'bold',
   },
   subjectText: {
     fontSize: 16,
@@ -213,7 +320,114 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     marginBottom: 20,
-    marginRight:30,
+    marginRight: 30,
+  },
+  skilevel: {
+    marginRight: 0,
+    fontSize: 15,
+  },
+  categori: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  badge: {
+    borderRadius: 4,
+    width: 'auto',
+    marginRight: 15,
+
+  },
+  subjectImage: {
+    width: 50,
+    height: 50,
+    marginRight: 12,
+  },
+
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)'
+
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 16,
+    borderRadius: 8,
+    width: '100%',
+    height: '100%',
+  },
+  modalTeacherImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginTop: 60,
+
+  },
+  modalItemText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  modalSubjectText: {
+    fontSize: 18,
+    marginTop: 10,
+  },
+  modalCloseButton: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    padding: 10,
+    zIndex: 1,
+    marginTop: 40,
+  },
+  modalCloseButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  modalinfoimage: {
+    alignItems: 'center',
+
+  },
+  yaks: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 15,
+  },
+  carrers: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 20,
+  },
+  teams: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 30,
+  },
+  yak: {
+    marginTop:10,
+  },
+  carrer: {
+    marginTop:10,
+  },
+  team: {
+    marginTop:10,
+  },
+  Imageforteacher:{
+    marginTop:20,
+    width: 190,
+    height: 190,
+  },
+  swiperSlide: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+  swiperImage: {
+    width: '100%',
+    height: '110%',
+    borderRadius:5,
+    marginTop:40
   },
 });
 
