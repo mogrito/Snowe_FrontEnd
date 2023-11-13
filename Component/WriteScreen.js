55bfcdc160c947dca61cbe32b6a73f7a42215a6f
hong <juseong0625@gmail.com>
2023-11-13 17:42:21 +0900

    edit 11-13 / 5:42

--- Component/WriteScreen.js
+++ Component/WriteScreen.js
@@ -3,10 +3,10 @@ import React, { useEffect, useState, useRef } from 'react';
 import { StyleSheet, KeyboardAvoidingView, View, Platform, Button, TextInput, Modal, Text, TouchableOpacity, Pressable, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { MaterialIcons } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker';
 import TransparentCircleButton from './TransparentCircleButton';
 import { getTokenFromLocal } from './TokenUtils';
 import axios from 'axios';
-
 
 const URL = 'http://192.168.25.204:8080';
 
@@ -202,7 +202,7 @@ function WriteScreen({ route }) {
        {/* <View style={{ flex: 1, alignItems: 'left', justifyContent: 'center' }}> */}
           <TouchableOpacity onPress={uploadImage}>
             <MaterialIcons name='add-a-photo' size={30} color="black" />
-            <Image source={{ uri: imageUrl }} style={{ width: 200, height: 200 }} />
+            <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
           </TouchableOpacity>
         {/* </View> */}
 
